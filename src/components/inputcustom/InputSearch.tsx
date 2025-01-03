1886ffd3b4c17402f3bd80c839e9f1b1b43665e7
NNQA <anhla12h@gmail.com>
2025-01-03 16:45:22 +0700

    change property input search

--- src/components/inputcustom/InputSearch.tsx
+++ src/components/inputcustom/InputSearch.tsx
@@ -11,89 +11,13 @@ interface PropsValue {
   text: string;
 }
 export interface InputSearchProps extends InputCustomProps {
-  isOpen?: boolean;
-  // setIsOpen: Dispatch<SetStateAction<boolean>>;
-  valueInput?: string;
-  setValueInput: Dispatch<SetStateAction<string>>;
-  handleOnChange: React.ChangeEventHandler<HTMLInputElement> | undefined;
-  items: PropsValue[];
   align?: "center" | "end" | "start";
   side?: "top" | "right" | "bottom" | "left" | undefined;
-  classNameSroll?: string;
 }
 
 export const InputSearch = React.forwardRef<HTMLInputElement, InputSearchProps>(
-  (
-    {
-      className,
-      isOpen,
-      // setIsOpen,
-      valueInput,
-      setValueInput,
-      handleOnChange,
-      items,
-      align,
-      side,
-      classNameSroll,
-      ...props
-    },
-    ref
-  ) => {
-    return (
-      <div>
-        {/* <Popover open={isOpen} onOpenChange={setIsOpen}> */}
-        {/* <PopoverTrigger className="w-full"> */}
-        <InputCustomIcon
-          ref={ref}
-          {...props}
-          className={className}
-          value={valueInput}
-          onChange={handleOnChange}
-        />
-        {/* </PopoverTrigger> */}
-        {/* <PopoverContent align={align} side={side}> */}
-        {/* <ScrollArea className={cn("h-[200px]", classNameSroll)}> */}
-        {/* ( */}
-        {/* {items.length > 0 ? (
-                items.map((item, index: number) => (
-                  <div
-                    key={index}
-                    className="p-2 hover:bg-primary hover:text-accent outline-none rounded-md"
-                    onClick={() => {
-                      setValueInput(item);
-                    }}
-                  >
-                    {item.name}
-                  </div>
-                ))
-                <div>
-                </div>
-              ) : (
-                <div>
-                  <Transition
-                    show={false}
-                    enter="transition ease-in-out"
-                    enterFrom="opacity-0 scale-0"
-                    leave="transition ease-in-out duration-300"
-                    leaveTo="opacity-0 scale-0"
-                  >
-                    <div className="w-[30px] h-[50px] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-                      <div className="flex flex-col items-center justify-center mb-20">
-                        <ProgressCircle
-                          className="w-[30px] h-[50px] text-primary"
-                          aria-label="signing in"
-                        />
-                        <p>Loading......</p>
-                      </div>
-                    </div>
-                  </Transition>
-                </div>
-              )}
-            </ScrollArea> */}
-        {/* </PopoverContent> */}
-        {/* </Popover> */}
-      </div>
-    );
+  ({ className, align, side, ...props }, ref) => {
+    return <InputCustomIcon ref={ref} {...props} className={className} />;
   }
 );
 
