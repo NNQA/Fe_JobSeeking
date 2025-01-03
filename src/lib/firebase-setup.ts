e5493ff084c0800086750cd99efd7dd975927bc4
NNQA <anhla12h@gmail.com>
2025-01-03 18:02:30 +0700

    set up env

--- src/lib/firebase-setup.ts
+++ src/lib/firebase-setup.ts
@@ -1,22 +1,17 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDXml0ozblxEoG_j48LQUG5NYQeMG4jJXk",
-  authDomain: "meals-ad52b.firebaseapp.com",
-  databaseURL: "https://meals-ad52b-default-rtdb.firebaseio.com",
-  projectId: "meals-ad52b",
-  storageBucket: "meals-ad52b.firebasestorage.app",
-  messagingSenderId: "22191584799",
-  appId: "1:22191584799:web:5dfd7410c85e9a2f90c72a",
-  measurementId: "G-E4HJE911WD"
+export const firebaseConfig = {
+  apiKey: "AIzaSyD-KEpD9bxu76YavDXwNOOHF5k16fEfJmg",
+  authDomain: "mobileappmusicplay.firebaseapp.com",
+  databaseURL: "https://mobileappmusicplay-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "mobileappmusicplay",
+  storageBucket: "mobileappmusicplay.appspot.com",
+  messagingSenderId: "354094234656",
+  appId: "1:354094234656:web:8756e1e7e5ba1e61b59045",
+  measurementId: "G-FM57ER2BGY"
 };
-
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
