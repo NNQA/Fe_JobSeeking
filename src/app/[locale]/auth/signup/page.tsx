16ae63c66b490647dbb38531b0631e8492798b87
NNQA <anhla12h@gmail.com>
2025-03-14 15:24:52 +0700

    refactor(auth): add responsive for page

--- src/app/[locale]/auth/signup/page.tsx
+++ src/app/[locale]/auth/signup/page.tsx
@@ -4,8 +4,7 @@ import FormSignup from "./FormSignUp";
 export default function Page() {
   return (
     <div className="relative h-screen w-screen overflow-hidden">
-      {/* Background image with overlay */}
-      <div className="absolute inset-0 -z-10">
+      <div className="absolute inset-0  -z-10">
         <Image
           src="/aaa.jpg"
           alt="Image of tropical jungle"
@@ -17,8 +16,7 @@ export default function Page() {
         <div className="absolute inset-0 bg-black bg-opacity-60"></div>
       </div>
 
-      {/* Content container */}
-      <div className="h-full w-full flex flex-col md:flex-row justify-center items-center gap-6 md:gap-16 z-10 px-4">
+      <div className="h-full flex flex-col lg:flex-row justify-center items-center lg:gap-[30rem] gap-2 z-10 px-4">
         <div className="text-accent text-2xl md:text-3xl font-bold order-1 md:order-1">
           JOB SEEKING
         </div>
