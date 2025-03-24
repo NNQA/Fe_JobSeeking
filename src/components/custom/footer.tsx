cee15015bbaf8497a413d5a9518dea5e945baf7b
NNQA <anhla12h@gmail.com>
2025-03-24 09:13:03 +0700

    refator(ui): add responsive for footer

--- src/components/custom/footer.tsx
+++ src/components/custom/footer.tsx
@@ -17,45 +17,47 @@ import Link from 'next/link'
 
 export function HoverCardDemo() {
   return (
-    <div className='text-primary'>
-      <HoverCard >
-        <HoverCardTrigger asChild>
-          <Button asChild variant={'link'} className='outline-none p-0'>
-            <Link href="https://github.com/">Github</Link>
-          </Button>
-        </HoverCardTrigger>
-        <HoverCardContent className="w-60">
-          <div className="flex justify-between space-x-4">
-            <Avatar>
-              <AvatarImage src="https://github.com/vercel.png" />
-              <AvatarFallback>VC</AvatarFallback>
-            </Avatar>
-            <div className="space-y-1">
-              <h4 className="text-sm font-semibold">QuocAnh</h4>
-              <p className="text-sm">
-                The developer Website
-              </p>
-              <div className="flex items-center pt-2">
-                <CalendarIcon className="mr-2 h-4 w-4 opacity-70" />{" "}
-                <span className="text-xs text-muted-foreground">
-                  Joined December 2021
-                </span>
-              </div>
+    <HoverCard >
+      <HoverCardTrigger asChild>
+        <Link href="https://github.com/NNQAs" className='text-primary underline-offset-4 hover:underline'>Github</Link>
+      </HoverCardTrigger>
+      <HoverCardContent className="w-60">
+        <div className="flex justify-between space-x-4">
+          <Avatar>
+            <AvatarImage src="https://github.com/vercel.png" />
+            <AvatarFallback>VC</AvatarFallback>
+          </Avatar>
+          <div className="space-y-1">
+            <h4 className="text-sm font-semibold">QuocAnh</h4>
+            <p className="text-sm">
+              The developer Website
+            </p>
+            <div className="flex items-center pt-2">
+              <CalendarIcon className="mr-2 h-4 w-4 opacity-70" />{" "}
+              <span className="text-xs text-muted-foreground">
+                Joined December 2021
+              </span>
             </div>
           </div>
-        </HoverCardContent>
-      </HoverCard>
-    </div>
+        </div>
+      </HoverCardContent>
+    </HoverCard>
   )
 }
 
 
 function Footer() {
   return (
-    <footer className="w-screen h-[3rem] pb-3 pt-1 px-20 bg-background border-t flex justify-between">
+    <footer className="w-screen md:h-[3rem] h-5 pb-3 md:pt-1 md:px-20 px-10 bg-background border-t md:flex md:justify-between md:items-center grid grid-flow-row">
       <div>
-        <div className='font-semibold flex items-center gap-1'>Built by <span className='text-xs text-primary'>WuokAnk
-        </span>. The service contained in my website is available on <HoverCardDemo />.</div>
+        <div className='font-semibold text-sm flex md:items-center gap-1'>
+          <p>
+            Built by <span className='text-primary'>WuokAnk</span>
+          </p>
+          <p className='space-x-2'>
+            The service contained in my website is available on <HoverCardDemo />
+          </p>
+        </div>
       </div>
       <div className="flex gap-6">
         <Button asChild variant={'link'} className='outline-none p-0'>
