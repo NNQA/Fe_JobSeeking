4b39fe8b548eb8607d0588a38b544607844ef9a4
NNQA <anhla12h@gmail.com>
2025-04-01 19:09:33 +0700

    add mocks categories

---
+++ src/app/mocks/Categories.ts
@@ -0,0 +1,94 @@
+import { JobCategories } from "@/lib/models/Categories";
+
+export const categoriesData: JobCategories[] = [
+  {
+    category: "Accounting & Consulting",
+    field: [
+      { name: "Personal & Professional Coaching" },
+      { name: "Accounting & Bookkeeping" },
+      { name: "Financial Planning" },
+      { name: "Recruiting & Human Resources" },
+      { name: "Management Consulting & Analysis" },
+      { name: "Other - Accounting & Consulting" },
+    ],
+  },
+  {
+    category: "Admin Support",
+    field: [
+      { name: "Virtual Assistance" },
+      { name: "Data Entry" },
+      { name: "Project Management" },
+      { name: "Customer Support" },
+      { name: "Other - Admin Support" },
+    ],
+  },
+  {
+    category: "Customer Service",
+    field: [
+      { name: "Technical Support" },
+      { name: "Customer Support" },
+      { name: "Call Center Operations" },
+      { name: "Other - Customer Service" },
+    ],
+  },
+  {
+    category: "Data Science & Analytics",
+    field: [
+      { name: "Data Analysis" },
+      { name: "Machine Learning" },
+      { name: "Data Visualization" },
+      { name: "Business Intelligence" },
+      { name: "Other - Data Science & Analytics" },
+    ],
+  },
+  {
+    category: "Design & Creative",
+    field: [
+      { name: "Graphic Design" },
+      { name: "UI/UX Design" },
+      { name: "Video Editing" },
+      { name: "Illustration" },
+      { name: "Other - Design & Creative" },
+    ],
+  },
+  {
+    category: "Engineering & Architecture",
+    field: [
+      { name: "Civil Engineering" },
+      { name: "Mechanical Engineering" },
+      { name: "Architectural Design" },
+      { name: "Electrical Engineering" },
+      { name: "Other - Engineering & Architecture" },
+    ],
+  },
+  {
+    category: "IT & Networking",
+    field: [
+      { name: "System Administration" },
+      { name: "Network Security" },
+      { name: "Cloud Computing" },
+      { name: "DevOps" },
+      { name: "Other - IT & Networking" },
+    ],
+  },
+  {
+    category: "Legal",
+    field: [
+      { name: "Contract Law" },
+      { name: "Corporate Law" },
+      { name: "Intellectual Property" },
+      { name: "Legal Research" },
+      { name: "Other - Legal" },
+    ],
+  },
+  {
+    category: "Sales & Marketing",
+    field: [
+      { name: "Digital Marketing" },
+      { name: "SEO" },
+      { name: "Content Marketing" },
+      { name: "Sales Strategy" },
+      { name: "Other - Sales & Marketing" },
+    ],
+  },
+];
