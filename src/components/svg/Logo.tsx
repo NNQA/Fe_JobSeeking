236d70fb29d752e67bdad4bf9411d3564d739293
NNQA <anhla12h@gmail.com>
2025-05-26 16:36:00 +0700

    change logo

--- src/components/svg/Logo.tsx
+++ src/components/svg/Logo.tsx
@@ -1,22 +1,12 @@
-import clsx from "clsx";
-import Image from "next/image";
+import { GalleryVerticalEnd } from "lucide-react";
 
 export default function Logo({ className }: { className?: string }) {
     return (
-        <div className="z-20 flex items-center gap-2">
-            <Image
-                src="/logo.svg"
-                alt="Logo"
-                width={30}
-                height={30}
-                className={clsx("dark:text-secondary-foreground dark:bg-white dark:border dark:rounded-full", className)}
-            />
-            <div className="bg-secondary-foreground/90 dark:bg-secondary rounded-sm px-3 py-2 h-8 flex items-center justify-center">
-                <h5 className="font-bold text-secondary dark:text-secondary-foreground drop-shadow-lg tracking-wide">
-                    MTA
-                </h5>
+        <div className="flex items-center gap-2 self-center font-medium">
+            <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+                <GalleryVerticalEnd className="size-4" />
             </div>
+            NNQA.
         </div>
-
     );
 }
