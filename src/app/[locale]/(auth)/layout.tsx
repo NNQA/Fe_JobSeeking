ff7abaf2ad09b393926a125b3fd81f37ac849086
NNQA <anhla12h@gmail.com>
2025-05-26 16:34:13 +0700

    refactor(auth): change layout for main ui (auth)

--- src/app/[locale]/(auth)/layout.tsx
+++ src/app/[locale]/(auth)/layout.tsx
@@ -1,7 +1,19 @@
-import Footer from "@/components/custom/footer";
+import Logo from "@/components/svg/Logo";
+import { cn } from "@/lib/utils";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <section>{children}
-    <Footer />
+  return <section>
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+      <div className="flex w-full max-w-[500px] flex-col gap-6">
+        <Logo />
+        <div className={cn("flex flex-col gap-6")}>
+          {children}
+          <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a]:hover:text-primary  ">
+            By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
+            and <a href="#">Privacy Policy</a>.
+          </div>
+        </div>
+      </div>
+    </div>
   </section>;
 }
